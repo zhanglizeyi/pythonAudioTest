7e2ba8d7bbcf6e16bb64259f2db13f04c196b9e2
Meiyi He <mehe@ucsd.edu>
2016-11-14 18:17:46 -0800

    merging

--- textProcess.py
+++ textProcess.py
@@ -1,5 +1,4 @@
 import sys
-#import hashTable
 import string, os
 import re
 from collections import Counter
@@ -10,23 +9,33 @@ from collections import Counter
 def analyzer(ifile):
 	global tokens
 
+	global hashTable
+
 
 	file = open(ifile, "r")
-	file = file.readlines();
-	tokens = []
-	for i in file:
-		tokens.append([e.lower() for e in map(string.strip, re.split("(\W+)",i)) if len(e) > 0 and not re.match("\W", e)])
+	file = file.read();
+	tokens = [e.lower() for e in map(string.strip, re.split("(\W+)",file)) if len(e) > 0 and not re.match("\W", e)]
+	#tokens = []
+	#for i in file:
+		#tokens.append([e.lower() for e in map(string.strip, re.split("(\W+)",i)) if len(e) > 0 and not re.match("\W", e)])
+		#tokens = [e.lower() for e in map(string.strip, re.split("(\W+)",file)) if len(e) > 0 and not re.match("\W", e)]
 	print tokens
 
 #def storeHash(tokens):
 	
+
+
+
 if __name__ == "__main__":
 	#print "Input file path:"
-	ifile = raw_input("Which file you want to process? ");
-	analyzer(ifile)
+	#ifile = raw_input("Which file you want to process? ");
+	analyzer("testTxt1.txt")
 	print
 	print
+
 	dictionary = {}
 	for i in range(10):
 		dictionary[i] = chr(ord('a') + i)
 	print dictionary
+
+	dictionary = {}
